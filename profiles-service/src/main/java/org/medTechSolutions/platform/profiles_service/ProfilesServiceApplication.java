961c04652f07aa87864deb2dfe07062274432c34
MiquelDlp <u202112179@upc.edu.pe>
2024-09-18 23:32:02 -0500

    feat: shared package implementation and pom configuration

--- profiles-service/src/main/java/org/medTechSolutions/platform/profiles_service/ProfilesServiceApplication.java
+++ profiles-service/src/main/java/org/medTechSolutions/platform/profiles_service/ProfilesServiceApplication.java
@@ -2,9 +2,11 @@ package org.medTechSolutions.platform.profiles_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class ProfilesServiceApplication {
+@EnableJpaAuditing
+public class  ProfilesServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfilesServiceApplication.class, args);
