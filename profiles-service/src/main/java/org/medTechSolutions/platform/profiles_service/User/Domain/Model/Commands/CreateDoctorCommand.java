7808e74dcf227bd2e6e3b98500747f2d9cd2863b
IsaacAmp24 <ampudiaflores@gmail.com>
2024-09-23 01:57:58 -0500

    feat(profiles-service): relationship with lab id removed.

--- profiles-service/src/main/java/org/medTechSolutions/platform/profiles_service/User/Domain/Model/Commands/CreateDoctorCommand.java
+++ profiles-service/src/main/java/org/medTechSolutions/platform/profiles_service/User/Domain/Model/Commands/CreateDoctorCommand.java
@@ -1,4 +1,12 @@
 package org.medTechSolutions.platform.profiles_service.User.Domain.Model.Commands;
 
-public record CreateDoctorCommand(String firstName, String lastName, String specialization, Integer licenceNumber , String phone, String email, Long idLaboratory) {
+public record CreateDoctorCommand(
+        String firstName,
+        String lastName,
+        String specialization,
+        Integer licenceNumber ,
+        String phone,
+        String email
+        //Long idLaboratory
+) {
 }
