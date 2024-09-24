cbedf9d83a2534ad0eb2083744a09885ede55b10
IsaacAmp24 <ampudiaflores@gmail.com>
2024-09-24 17:05:14 -0500

    refactor(appointment-service): class Appointment date refactored.

--- appointments-service/src/main/java/org/medTechSolutions/platform/appointments_service/Appointments/Domain/Model/Aggregates/Appointment.java
+++ appointments-service/src/main/java/org/medTechSolutions/platform/appointments_service/Appointments/Domain/Model/Aggregates/Appointment.java
@@ -1,17 +1,29 @@
 package org.medTechSolutions.platform.appointments_service.Appointments.Domain.Model.Aggregates;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 import org.medTechSolutions.platform.appointments_service.Appointments.Domain.Model.Commands.CreateAppointmentCommand;
 import org.medTechSolutions.platform.appointments_service.Shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+import java.time.LocalDate;
+
 @Entity
 @Getter
+@Setter
 public class Appointment extends AuditableAbstractAggregateRoot<Appointment> {
+
+    @NotNull(message = "Doctor ID is required")
     private Long doctorId;
+
+    @NotNull(message = "Patient ID is required")
     private Long patientId;
 
-    private String date;
+    @NotNull(message = "Date is required")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "America/Lima")
+    private LocalDate date;
 
     private String reason;
 
@@ -29,7 +41,7 @@ public class Appointment extends AuditableAbstractAggregateRoot<Appointment> {
         return this;
     }
 
-    public Appointment updateDate(String date) {
+    public Appointment updateDate(LocalDate date) {
         this.date = date;
         return this;
     }
